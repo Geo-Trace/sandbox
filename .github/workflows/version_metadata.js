5eb6c64278e1775b5f991e79493961ef3c36865f
nacim.h <haiahem.nacim@gmail.com>
2022-09-15 12:01:31 +0200

    ci: installation du versioning :construction_worker:

---
+++ .github/workflows/version_metadata.js
@@ -0,0 +1,13 @@
+{
+    "files": 
+        {
+            "path/file1":
+            {
+                "version": "1.01"
+            },
+            "path2/file2": 
+            {
+            "version": "1.01"
+            }
+        }
+}
