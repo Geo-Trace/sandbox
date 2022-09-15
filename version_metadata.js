95c6cba650e204fa096702d7fdab94e7cb293449
nacim.h <haiahem.nacim@gmail.com>
2022-09-15 13:44:38 +0200

    ci(fix): installation du versioning (fix version_metadata.js) :construction_worker:

--- version_metadata.js
+++ version_metadata.js
@@ -1,13 +1,11 @@
 {
-    "files": 
+    "files"; 
         {
-            "path/file1":
-            {
-                "version": "1.01"
-            },
-            "path2/file2": 
-            {
-            "version": "1.01"
+            "path/file1";{
+            "version"; "1.01"
+            };
+            "path2/file2"; {
+            "version"; "1.01"
             }
         }
 }
